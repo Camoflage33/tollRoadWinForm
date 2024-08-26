cf0eaec9a7e7c50dd76c84080242ec62ba58981f
Camo Flage <youngprodig54@gmail.coom>
2024-08-26 10:42:45 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace tollRoadWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vehicleWeight = new System.Windows.Forms.Label();
+            this.tollCost = new System.Windows.Forms.Label();
+            this.roadCap = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vehicleWeight
+            // 
+            this.vehicleWeight.AutoSize = true;
+            this.vehicleWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vehicleWeight.Location = new System.Drawing.Point(62, 47);
+            this.vehicleWeight.Name = "vehicleWeight";
+            this.vehicleWeight.Size = new System.Drawing.Size(138, 24);
+            this.vehicleWeight.TabIndex = 0;
+            this.vehicleWeight.Text = "Vehicle Weight";
+            // 
+            // tollCost
+            // 
+            this.tollCost.AutoSize = true;
+            this.tollCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tollCost.Location = new System.Drawing.Point(62, 101);
+            this.tollCost.Name = "tollCost";
+            this.tollCost.Size = new System.Drawing.Size(83, 24);
+            this.tollCost.TabIndex = 0;
+            this.tollCost.Text = "Toll Cost";
+            // 
+            // roadCap
+            // 
+            this.roadCap.AutoSize = true;
+            this.roadCap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roadCap.Location = new System.Drawing.Point(62, 162);
+            this.roadCap.Name = "roadCap";
+            this.roadCap.Size = new System.Drawing.Size(131, 24);
+            this.roadCap.TabIndex = 0;
+            this.roadCap.Text = "Road Capacity";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(252, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(252, 162);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(66, 225);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(94, 30);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.roadCap);
+            this.Controls.Add(this.tollCost);
+            this.Controls.Add(this.vehicleWeight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label vehicleWeight;
+        private System.Windows.Forms.Label tollCost;
+        private System.Windows.Forms.Label roadCap;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tollRoadWinForm
+{
+    public partial class Form1 : Form
+    {
+        private object lstResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {          
+
+            // Knapsack algorithm adapted from previous code
+           int MaximizeVehicleFlow(int[] vehicleWeights, int[] vehicleValues, int roadCapacity)
+            {
+                int n = vehicleWeights.Length;
+                int[,] dp = new int[n + 1, roadCapacity + 1];
+
+                for (int i = 0; i <= n; i++)
+                {
+                    for (int w = 0; w <= roadCapacity; w++)
+                    {
+                        if (i == 0 || w == 0)
+                            dp[i, w] = 0;
+                        else if (vehicleWeights[i - 1] <= w)
+                            dp[i, w] = Math.Max(vehicleValues[i - 1] + dp[i - 1, w - vehicleWeights[i - 1]], dp[i - 1, w]);
+                        else
+                            dp[i, w] = dp[i - 1, w];
+                    }
+                }
+
+                return dp[n, roadCapacity];
+            }
+
+           void DisplayResult(int[] vehicleWeights, int[] vehicleValues, int roadCapacity)
+            {
+                int n = vehicleWeights.Length;
+                bool[,] keep = new bool[n + 1, roadCapacity + 1];
+                int[,] dp = new int[n + 1, roadCapacity + 1];
+
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int w = 0; w <= roadCapacity; w++)
+                    {
+                        if (vehicleWeights[i - 1] <= w && vehicleValues[i - 1] + dp[i - 1, w - vehicleWeights[i - 1]] > dp[i - 1, w])
+                        {
+                            dp[i, w] = vehicleValues[i - 1] + dp[i - 1, w - vehicleWeights[i - 1]];
+                            keep[i, w] = true;
+                        }
+                        else
+                        {
+                            dp[i, w] = dp[i - 1, w];
+                        }
+                    }
+                }
+
+                int K = roadCapacity;
+                lstResult.Items.Clear();
+                lstResult.Items.Add("Selected vehicles for optimal flow:");
+                for (int i = n; i > 0; i--)
+                {
+                    if (keep[i, K])
+                    {
+                        lstResult.Items.Add($"Vehicle Type {i} (Weight: {vehicleWeights[i - 1]}, Value: {vehicleValues[i - 1]})");
+                        K -= vehicleWeights[i - 1];
+                    }
+                }
+            }
+
+            private void btnCalculate_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    int[] vehicleWeights = Array.ConvertAll(txtVehicleWeights.Text.Split(','), int.Parse);
+                    int[] vehicleValues = Array.ConvertAll(txtVehicleValues.Text.Split(','), int.Parse);
+                    int roadCapacity = int.Parse(txtRoadCapacity.Text);
+
+                    int maxFlowValue = MaximizeVehicleFlow(vehicleWeights, vehicleValues, roadCapacity);
+                    lstResult.Items.Add($"Maximum Value of Vehicle Flow: {maxFlowValue}");
+                    lstResult.Items.Add("");
+
+                    DisplayResult(vehicleWeights, vehicleValues, roadCapacity);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+
+}
+    
+
